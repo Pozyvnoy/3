77022f4c652592a664c7d264d4be5bc78a860d87
Pozyvnoy <92524889+Pozyvnoy@users.noreply.github.com>
2021-11-02 14:14:45 +0300

    Update lab2_5.py

--- lab2/lab2_5.py
+++ lab2/lab2_5.py
@@ -1,6 +1,17 @@
-n = float(input("n = "))
-for x in range(n + 1):
-    for y in range(n + 1):
-         if (x*x+y*y)==n :
-            print(x,y)
-            print(y,x)
+print("N = ")
+N=int(input())
+k=0
+for x in range(N):
+    if(x**3>N):
+        break
+    for y in range(N):
+        if(y**3+x**3>N):
+            break
+        for z in range(N):
+            if(y**3+x**3+z**3>N):
+                break
+            if((x**3)+(y**3)+(z**3)==N):
+                print(x,y,z)
+                k+=1
+if(k==0):
+    print('No such combinations')
