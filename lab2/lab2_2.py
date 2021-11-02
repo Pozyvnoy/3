6388b7b131a1366beeb110b465091e17338bc33d
Pozyvnoy <92524889+Pozyvnoy@users.noreply.github.com>
2021-11-02 14:13:50 +0300

    Update lab2_2.py

--- lab2/lab2_2.py
+++ lab2/lab2_2.py
@@ -1,4 +1,13 @@
-import math
 print("Введите количество слагаемых после Pi/4: ")
-count = float(input())
-if count >= 1
+n = int(input())
+n = n - 1
+s = 1
+l = 3
+for i in range(n):
+        if i % 2 == 0:
+            s = s - 1 / l
+        else:
+            s = s + 1 / l
+l += 2
+s = s * 4
+print(s)
